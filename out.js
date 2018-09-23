3eecc10f4654861257793911f14085e92c76dc20
Hannah37 <hannah3767@gmail.com>
2018-09-23 00:39:24 -0400

    3d scatter plot

---
+++ out.js
@@ -0,0 +1,39 @@
+$.ajax({
+    url: 'output.csv',
+    dataType: 'text',
+  }).done(successFunction);
+
+  function successFunction(data) {
+    var allRows = data.split(/\r?\n|\r/);
+    var table = '<table>';
+    for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+      if (singleRow === 0) {
+        table += '<thead>';
+        table += '<tr>';
+      } else {
+        table += '<tr>';
+      }
+      var rowCells = allRows[singleRow].split(',');
+      for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+        if (singleRow === 0) {
+          table += '<th>';
+          table += rowCells[rowCell];
+          table += '</th>';
+        } else {
+          table += '<td>';
+          table += rowCells[rowCell];
+          table += '</td>';
+        }
+      }
+      if (singleRow === 0) {
+        table += '</tr>';
+        table += '</thead>';
+        table += '<tbody>';
+      } else {
+        table += '</tr>';
+      }
+    } 
+    table += '</tbody>';
+    table += '</table>';
+    $('body').append(table);
+  }
